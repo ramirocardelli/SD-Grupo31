7118e9b06b3d2c1c72178a20ffd4644f5912ac6c
ramirocardelli <ramirocardelli89@gmail.com>
2024-10-22 23:42:35 -0300

    Mejorar la legibilidad del código y actualizar comentarios
    - Refactorización del código para mejorar la claridad.
    - Modificación de comentarios (principalmente mayúsculas, tildes, etc.)
    - Un return antes de todos los res.end() para que no continúe la ejecución
    - Se agregaron algunos comentarios con observaciones y "TODO"

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ import {
   getAllAnimals,
   modAnimal,
   removeAnimal,
-  modAnimal,
 } from "./controllers/animals.controller.js";
 import {
   addCheckpoint,
@@ -14,7 +13,7 @@ import {
 } from "./controllers/checkpoints.controller.js";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-import { loginUser, registerUser } from "./controllers/user.controller.js";
+import { login } from "./controllers/user.controller.js";
 import {
   connectToBroker,
   getPosiciones,
@@ -23,248 +22,269 @@ import {
 const HTTP_PORT = process.env.PORT;
 const secret = process.env.SECRET;
 
-//nos conectamos al broker mqtt
+// Conexión con el broker MQTT
 connectToBroker();
 
-//raiz de la api
+// Raíz de la API
 const server = http.createServer((req, res) => {
-  res.writeHead(200, "Operacion exitosa");
-  //sea una solicitud get, post etc, siempre se intentara primero, reeconstruir
-  //el cuerpo de la solicitud
+  // Sea el tipo de solicitud que sea, siempre se intentara primero, reeconstruir el cuerpo de la solicitud
   let body = "";
   req.on("data", (chunk) => {
     body = body + chunk;
   });
 
+  let parsedBody;
+  if (body != "") {
+    parsedBody = JSON.parse(data);
+  }
+
   req.on("end", () => {
-    //ruta publica
+    // Rutas públicas
     if (req.url === "/login") {
-      onLogin(req, res, body);
-    } else {
-      if (tokenIsValid(req)) {
-        // Rutas privadas
-        // El sistema deberá permitir a los Administradores dar de alta, baja, modificar y mostrar Animales.
-        if (req.url === "/animals") {
-          onAnimals(req, res, body);
-        }
-        // El sistema deberá permitir a los Administradores dar de alta, baja, modificar y mostrar Puntos de Control
-        else if (req.url === "/checkpoints") {
-          onCheckpoints(req, res, body);
-        } else {
-          res.writeHead(404, "Path invalido");
-          res.end();
-        }
-      } else {
-        res.writeHead(401, "Token invalido");
-        res.end();
-      }
+      onLogin(req, res, parsedBody);
+      return;
     }
+
+    // Para las rutas privadas, si el tóken no es válido, no sigue
+    if (!tokenIsValid(req)) {
+      res.writeHead(401, "Token invalido");
+      return res.end();
+    }
+
+    // Rutas privadas
+
+    // El sistema deberá permitir a los Administradores dar de alta, baja, modificar y mostrar Animales.
+    if (req.url === "/animals") {
+      onAnimals(req, res, parsedBody);
+      return;
+    }
+
+    // El sistema deberá permitir a los Administradores dar de alta, baja, modificar y mostrar Puntos de Control
+    if (req.url === "/checkpoints") {
+      onCheckpoints(req, res, parsedBody);
+      return;
+    }
+
+    res.writeHead(404, "Path invalido");
+    return res.end();
   });
 });
 
-//analizamos si el token JWT del usuario es valido, lo podemos ver en el campo
-//"authoriation" del header de la solicitud, de la forma:
-// ["authorization"]="bearer [tokenJWT]"
+/*
+Analizamos si el token JWT del usuario es valido, lo podemos ver en el campo
+"authoriation" del header de la solicitud, de la forma:
+["authorization"]="bearer [tokenJWT]"
+*/
+
 function tokenIsValid(req) {
   const token = req.headers["authorization"]?.split(" ")[1];
   try {
-    //si es valido, jwt.verify no tira ninguna excepcion
+    // Si es válido, jwt.verify no lanza ninguna excepción
     const verify = jwt.verify(token, secret);
+    // TODO: Además de ser válido, debe no haber expirado
+    // TODO: Refresh Token
     return true;
   } catch (e) {
     return false;
   }
 }
 
-//asumimos que los datos de logeo vienen el el body de la solicitud
-//igualmente esto no es correcto, deberia venir en el header.
-function onLogin(req, res, data) {
-  let parsedBody;
-  if (data != "") {
-    parsedBody = JSON.parse(data);
-  }
+// Asumimos que los datos de logeo vienen el el body de la solicitud
+// Igualmente esto no es correcto, deberia venir en el header.
+function onLogin(req, res, body) {
+  const username = body?.name;
+  const password = body?.password;
 
-  const userClient = parsedBody["name"];
-  const passClient = parsedBody["password"];
-
-  if (!parsedBody?.name || !parsedBody?.password) {
+  if (!username || !password) {
     res.writeHead(400, "Credenciales de usuario invalidas");
-    res.end();
-    return;
+    return res.end();
   }
-  if (req.method === "GET") {
-    try {
-      //Si el logeo es correcto le devolvemos al usuario su token JWT
-      //de lo contrario se lanza una excepcion
-      //habria que generar un refresh token si el token inicial sigue en pie
-      //o por cada accion refrescar el token, VER
-      loginUser(userClient, passClient);
-      res.statusCode = 200;
-      res.end(JSON.stringify(tokenGenerator(userClient, passClient)));
-    } catch (e) {
-      res.writeHead(401, e.message);
-      res.end();
-    }
-  }
+
   if (req.method === "POST") {
     try {
-      registerUser(userClient, passClient);
-      res.end();
+      // Si el logeo es correcto le devolvemos al usuario su token JWT
+      // De lo contrario se lanza una excepcion
+      // TODO: Habria que generar un refresh token si el token inicial sigue en pie
+      // O por cada accion refrescar el token
+      login(username, password);
+      res.writeHead(200);
+      return res.end(JSON.stringify(tokenGenerator(username)));
     } catch (e) {
       res.writeHead(401, e.message);
-      res.end();
+      return res.end();
     }
   }
-  if (req.method != "GET" || req.method != "POST" || req.message != "PUT") {
-    res.writeHead(404, "Metodo invalido");
-    res.end();
+
+  // No hay registro, lo comento por si llegamos a agregarlo
+  /*
+  if (req.method === "POST") {
+    try {
+      register(username, password);
+      res.writeHead(200);
+      return res.end();
+    } catch (e) {
+      res.writeHead(401, e.message);
+      return res.end();
+    }
   }
+  */
+
+  res.writeHead(404, "Método invalido");
+  return res.end();
 }
 
-//hay que modificar la duracion del token
-function tokenGenerator(user, pass) {
+// TODO hay que modificar la duracion del token
+function tokenGenerator(user) {
   const data = {
-    sub: user,
-    name: pass,
+    user: user,
     //lo que le sumamos es la cantidad de segundos en la que es valido
     exp: Math.floor(Date.now() / 1000) + 3000,
   };
   return jwt.sign(data, secret);
 }
 
-function onAnimals(req, res, data) {
+function onAnimals(req, res, body) {
   if (req.method === "GET") {
+    const animals = getAllAnimals(req, res);
+    //le cargo las posiciones a los animales y las enviamos
+    // TODO: Esto no debería ser responsabilidad del controller / repository?
+    const posiciones = getPosiciones();
+    animals.forEach((animal) => {
+      animal.posicion = posiciones.get(animal.name);
+    });
+    res.writeHead(200);
+    return res.end(JSON.stringify(animals));
+  }
+
+  if (req.method === "POST") {
+    const name = body?.name;
+    const description = body?.description;
+
+    if (!name || !description) {
+      res.writeHead(400, "Credenciales del animal invalidas");
+      return res.end();
+    }
+
     try {
-      const resultado = getAllAnimals(req, res);
-      //le cargo las posiciones a los animales y las enviamos
-      const posicion = getPosiciones();
-      resultado.forEach((animal) => {
-        animal.posicion = posicion.get(animal.name);
-      });
-      res.end(JSON.stringify(resultado));
+      addAnimal(name, description);
+      res.writeHead(200, "Animal añadido con éxito");
+      return res.end();
     } catch (e) {
-      res.statusCode = 500;
-      res.end();
-    }
-  } else {
-    let parsedBody;
-    if (data != "") {
-      parsedBody = JSON.parse(data);
-    }
-    if (req.method === "POST") {
-      if (!parsedBody?.name || !parsedBody?.description) {
-        res.writeHead(400, "Credenciales del animal invalidas");
-        res.end();
-        return;
-      }
-      try {
-        addAnimal(parsedBody.name, parsedBody.description);
-        res.writeHead(200, "se creo el animal");
-        res.end();
-      } catch (e) {
-        res.writeHead(400, e.message);
-        res.end();
-      }
-    }
-    if (req.method === "PUT") {
-      try {
-        modAnimal(parsedBody.name, parsedBody.description);
-        res.writeHead(200, "se modifico el animal");
-        res.end();
-      } catch (e) {
-        res.writeHead(400, e.message);
-        res.end();
-      }
-    }
-    if (req.method === "DELETE") {
-      if (!parsedBody?.name) {
-        res.writeHead(400, "Credenciales del animal invalidas");
-        res.end();
-        return;
-      }
-      try {
-        removeAnimal(parsedBody.name);
-        res.writeHead(200, "se elimino el animal");
-        res.end();
-      } catch (e) {
-        res.writeHead(404, e.message);
-        res.end();
-      }
-    }
-    if (req.method != "POST" || req.method != "DELETE") {
-      res.writeHead(404, "Metodo invalido");
-      res.end();
+      res.writeHead(400, e.message);
+      return res.end();
     }
   }
-}
 
-function onCheckpoints(req, res, data) {
-  if (req.method === "GET") {
+  if (req.method === "PUT") {
+    const name = body?.name;
+    const description = body?.description;
+
+    if (!name || !description) {
+      res.writeHead(400, "Credenciales del animal invalidas");
+      return res.end();
+    }
+
     try {
-      const resultado = getAllCheckpoints(req, res);
-      res.end(JSON.stringify(resultado));
+      modAnimal(name, description);
+      res.writeHead(200, "El animal se modificó correctamente");
+      return res.end();
     } catch (e) {
-      res.statusCode = 500;
-      res.end();
-    }
-  } else {
-    let parsedBody;
-    if (data != "") {
-      parsedBody = JSON.parse(data);
-    }
-    if (req.method === "POST") {
-      if (!parsedBody?.name || !parsedBody?.description) {
-        res.writeHead(400, "credenciales de checkpoint invalida/s");
-        res.end();
-        return;
-      }
-      try {
-        addCheckpoint(parsedBody.name, parsedBody.description);
-        res.writeHead(200, "se creo el checkpoint");
-        res.end();
-      } catch (e) {
-        res.writeHead(400, e.message);
-        res.end();
-      }
-    }
-    if (req.method === "PUT") {
-      if (!parsedBody?.name || !parsedBody?.description) {
-        res.writeHead(400, "credenciales de checkpoint invalida/s");
-        res.end();
-        return;
-      }
-      try {
-        modCheckpoint(parsedBody.name, parsedBody.description);
-        res.writeHead(200, "se modifico el checkpoint");
-        res.end();
-      } catch (e) {
-        res.writeHead(400, e.message);
-        res.end();
-      }
-    }
-    if (req.method === "DELETE") {
-      if (!parsedBody?.name) {
-        res.writeHead(400, "credenciales de checkpoint invalida/s");
-        res.end();
-        return;
-      }
-      try {
-        removeCheckpoint(parsedBody.name);
-        res.writeHead(200, "checkpoint eliminado");
-        res.end();
-      } catch (e) {
-        res.writeHead(400, e.message);
-        res.end();
-      }
-    }
-    if (req.method != "POST" || req.method != "DELETE" || req.method != "PUT") {
-      res.writeHead(404, "Metodo invalido");
-      res.end();
+      res.writeHead(400, e.message);
+      return res.end();
     }
   }
+
+  if (req.method === "DELETE") {
+    // TODO: Creo que deberían eliminarse por uid, no por name
+    const name = body?.name;
+    if (!name) {
+      res.writeHead(400, "Credenciales del animal invalidas");
+      return res.end();
+    }
+
+    try {
+      removeAnimal(name);
+      res.writeHead(200, "Se eliminó el animal con éxito");
+      return res.end();
+    } catch (e) {
+      res.writeHead(400, e.message);
+      return res.end();
+    }
+  }
+
+  res.writeHead(404, "Metodo invalido");
+  return res.end();
 }
 
-//levantamos el server
+function onCheckpoints(req, res, body) {
+  if (req.method === "GET") {
+    const checkpoints = getAllCheckpoints(req, res);
+    res.writeHead(200);
+    return res.end(JSON.stringify(checkpoints));
+  }
+
+  if (req.method === "POST") {
+    const name = body?.name;
+    const description = body?.description;
+
+    if (!name || !description) {
+      res.writeHead(400, "Credenciales de checkpoint inválidas");
+      return res.end();
+    }
+
+    try {
+      addCheckpoint(name, description);
+      res.writeHead(200, "se creo el checkpoint");
+      return res.end();
+    } catch (e) {
+      res.writeHead(400, e.message);
+      return res.end();
+    }
+  }
+
+  if (req.method === "PUT") {
+    const name = body?.name;
+    const description = body?.description;
+
+    if (!name || !description) {
+      res.writeHead(400, "Credenciales de checkpoint inválidas");
+      return res.end();
+    }
+
+    try {
+      modCheckpoint(name, description);
+      res.writeHead(200, "Se modificó el checkpoint correctamente");
+      return res.end();
+    } catch (e) {
+      res.writeHead(400, e.message);
+      return res.end();
+    }
+  }
+
+  if (req.method === "DELETE") {
+    // TODO: Creo que deberían eliminarse por uid, no por name
+    const name = body?.name;
+
+    if (!name) {
+      res.writeHead(400, "credenciales de checkpoint invalida/s");
+      return res.end();
+    }
+
+    try {
+      removeCheckpoint(name);
+      res.writeHead(200, "checkpoint eliminado");
+      return res.end();
+    } catch (e) {
+      res.writeHead(400, e.message);
+      return res.end();
+    }
+  }
+
+  res.writeHead(404, "Metodo invalido");
+  return res.end();
+}
+
+// Levantamos el server
 server.listen(HTTP_PORT, () => {
   console.log(`Servidor escuchando en puerto ${HTTP_PORT}`);
 });
