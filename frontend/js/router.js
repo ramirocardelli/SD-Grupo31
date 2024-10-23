9151cec75b402c67cabe4a1c8e10bfa6043dfe0c
josefinafrasca <josefrasca16@gmail.com>
2024-10-23 20:56:26 -0300

    Cambios front
    -Modularizacion archivos css
    -Esta el esqueleto de gestion animales (dps es copy paste para gestion checkpoints) pero le estoy pifiando en algo porq depende si corro el live server directo con index.html o solo con animal.html, funciona bien o no. Despues investigo

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -6,23 +6,22 @@ const route = (event) => {
 };
 
 const routes = {
-  404: "/pages/404.html",
   "/": "/pages/index.html",
+  404: "/pages/404.html",
   "/about": "/pages/about.html",
   "/login": "/pages/login.html",
-  "/animal": "/pages/animal.html",
+  "/animal": "/pages/animal.html", 
   "/checkpoints": "/pages/checkpoints.html",
 };
 
 const handleLocation = async () => {
-  console.log("Anda!");
   const path = window.location.pathname;
-  const route = routes[path] || routes[404];
+  const route = routes[path] || routes[404]; // Usar 404 si no se encuentra la ruta
   const html = await fetch(route).then((data) => data.text());
   document.getElementById("main-page").innerHTML = html;
 };
 
-window.onpopstate = handleLocation;
-window.route = route;
+window.onpopstate = handleLocation; // Para manejar el retroceso del historial
+window.route = route; // Para que sea accesible en el HTML
 
-handleLocation();
+handleLocation(); 
