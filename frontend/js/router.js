6d05d56ec1c51fb73dcd38c373481cce34648766
josefinafrasca <josefrasca16@gmail.com>
2024-10-24 13:22:45 -0300

    index.html de pages ahora es home.html (asi no hay 2 index)
    intente cargar dinamicamente los css de cada pagina pero no (lo comentado en router.js)

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -6,7 +6,7 @@ const route = (event) => {
 };
 
 const routes = {
-  "/": "/pages/index.html",
+  "/": "/pages/home.html",
   404: "/pages/404.html",
   "/about": "/pages/about.html",
   "/login": "/pages/login.html",
@@ -19,6 +19,35 @@ const handleLocation = async () => {
   const route = routes[path] || routes[404]; // Usar 404 si no se encuentra la ruta
   const html = await fetch(route).then((data) => data.text());
   document.getElementById("main-page").innerHTML = html;
+
+  /*
+  // Cargar dinámicamente los estilos
+  const existingStyles = document.querySelectorAll('link[rel="stylesheet"]');
+  existingStyles.forEach(style => style.remove()); // Eliminar estilos previos
+
+  // CSS correspondiente
+  let cssFile = '';
+  if (path === '/') {
+    cssFile = 'style.css'; // Cambia por el estilo que desees para la página principal
+  } else if (path === '/animal') {
+    cssFile = 'animal_style.css';
+  } else if (path === '/login') {
+    cssFile = 'login_style.css';
+  } else if (path === '/checkpoints') {
+    cssFile = 'checkpoints_style.css';
+  } else if (path === '/about') {
+    cssFile = '404_style.css';
+  }
+
+  // Cargar el CSS correspondiente
+  if (cssFile) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = `css/${cssFile}`;
+    document.head.appendChild(link);
+  }
+
+  */
 };
 
 window.onpopstate = handleLocation; // Para manejar el retroceso del historial
