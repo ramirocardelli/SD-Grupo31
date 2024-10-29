7da36324c80e46c1e1eb21243e8a2c213d207b92
artizan3 <brunotrinitario@gmail.com>
2024-10-29 11:00:11 -0300

    [MQTT] Actualizacion de mqtt controller
    - Se adecuo la recepcion de mensajes a lo que definimos con la catedra
    - Se aniadio el usuario y contrasena para conectarse al broker

--- backend/controllers/mqtt.controller.js
+++ backend/controllers/mqtt.controller.js
@@ -1,21 +1,31 @@
 import mqtt from "mqtt";
-import { getAllAnimals } from "./animals.controller.js";
-const client = mqtt.connect("mqtt://localhost:1883");
-//mapa = {nombre_animal,[pos.x,pos.y]}
+import { getAllCheckpoints } from "./checkpoints.controller.js";
+const options = {
+  username: "admin",
+  password: "admin",
+  clientID: "prueba",
+};
+const client = mqtt.connect("mqtt://localhost:1883", options);
+//mapa = {checkpoint.id,[vector de animales]}
 const posiciones = new Map();
 
-//Creamos un mapa donde tiene para cada nombre de animal, sus posiciones en X e Y
-//la posicion inicial es 0,0, pero se va actualizando a medida que le llegan mensajes
-const animals = getAllAnimals();
-animals.forEach((animal) => {
-  const pos = [0, 0];
-  posiciones.set(animal.name, pos);
+//Creamos un mapa para cada checkpoint y el listado VACIO de los animales que pertencen a el
+const checkpoints = getAllCheckpoints();
+checkpoints.forEach((checkpoint) => {
+  const datos = {
+    id: checkpoint.id,
+    lat: checkpoint.lat,
+    long: checkpoint.long,
+    description: checkpoint.description,
+    animals: [],
+  };
+  posiciones.set(checkpoint.id, datos);
 });
 //esta funcion se ejecuta en el index, lo que hace es quedarse escuchando el topico y
 //se conecta al broker
 export function connectToBroker() {
   client.on("connect", () => {
-    client.subscribe("server", (err) => {
+    client.subscribe("checkpoint", (err) => {
       if (!err) {
       }
     });
@@ -26,26 +36,25 @@ export function connectToBroker() {
 export function getPosiciones() {
   return posiciones;
 }
-//imagino que el mensaje que se envia es para el topico "server" y
-//viene en forma de json tipo {animal:nombre, X:X,Y:Y}
-//y en base a esto guardarlo en algun archivo asi se puede leer la posicion real si
-//se consultara a la api tipo GET .../posicion para pensar
+//El mensaje esperado es un checkpoint ID y un vector de animales que pertenecen a el
 client.on("message", (topic, message) => {
   // message is Buffer
-  if (topic === "server") {
-    console.log(message.toString());
+  if (topic === "checkpoint") {
+    //console.log(message.toString());
     actualizarPosicion(message.toString());
-    console.log(posiciones);
+    //console.log(posiciones);
     //client.end();
   }
 });
 
-//si llega un mensaje con un animal y su posicion, se actualiza en el mapa
+//si llega un mensaje con un checkpoint y los animales que pertencen a el, se actualiza en el mapa
 function actualizarPosicion(mensaje) {
   mensaje = JSON.parse(mensaje);
-  if (posiciones.has(mensaje.name)) {
-    const pos = [mensaje.x, mensaje.y];
-    posiciones.set(mensaje.name, pos);
-  } else {
+  if (posiciones.has(mensaje.checkpointID)) {
+    posiciones.get(mensaje.checkpointID).animals = mensaje.animals;
   }
 }
+
+client.on("error", (err) => {
+  //TODO que pasa si hay un error entre las raspberry y el broker
+});
