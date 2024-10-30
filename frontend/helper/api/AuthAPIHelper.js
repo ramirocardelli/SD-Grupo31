b6dd9e378d8fb3382383ef2d26ea443df184fb77
josefinafrasca <josefrasca16@gmail.com>
2024-10-30 18:23:53 -0300

    [AuthAPIHelper] Faltaba importar axios je

--- frontend/helper/api/AuthAPIHelper.js
+++ frontend/helper/api/AuthAPIHelper.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class AuthAPIHelper {
     static async login({ id, password }) {
         const response = await axios.post('API/login', { id, password });
