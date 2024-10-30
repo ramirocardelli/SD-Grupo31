14a33de193e75f4c8ce94a82567a03224467960d
josefinafrasca <josefrasca16@gmail.com>
2024-10-30 17:09:16 -0300

    Cambios login
    - Cambios para obtener el username y password correctamente

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -6,10 +6,10 @@ import AuthStateHelper from '../helper/state/AuthStateHelper.js';
 async function handleLogin(event) {
     try {
         event.preventDefault();
-        const username = document.getElementById("username").value.trim();
-        const password = document.getElementById("password").value.trim();
+        const username = event.target.elements.username.value.trim();
+        const password = event.target.elements.password.value.trim();
 
-        const userData = await AuthAPIHelper.login({ username, password });
+        const userData = await AuthAPIHelper.login({ username, password }); //enviar solicitud a la API - retorna tokens
             const { accessToken, refreshToken, ...rest } = userData;
             UserStateHelper.setUser(rest);
             AuthStateHelper.setAuth({ accessToken, refreshToken })
