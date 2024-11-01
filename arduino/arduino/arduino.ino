76cff4201f34cc826a2f33ee3ade4f4c7359a817
juaaniolave <juaaniolave@gmail.com>
2024-11-01 15:58:53 -0300

    Update arduino.ino

--- arduino/arduino/arduino.ino
+++ arduino/arduino/arduino.ino
@@ -6,10 +6,14 @@
 #include <BLEAdvertisedDevice.h>
 
 // Constants
-const char* ssid = "XXXXXXX";
-const char* password = "XXXXXXXX";
-const char* mqtt_server = "192.168.0.142";
+const char* ssid = "Asustado potter??";
+const char* password = "niunpoco!";
+const char* mqtt_server = "192.168.153.90";
 const int mqtt_port = 1883;
+const char* mqtt_user = "admin";    // Usuario para MQTT
+const char* mqtt_password = "admin"; // Contraseña para MQTT
+
+String macAddress;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -20,8 +24,6 @@ const int scanTime = 10; // In seconds
 void setup() {
   Serial.begin(115200);
 
-   macAddress = WiFi.macAddress();
-
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -30,18 +32,24 @@ void setup() {
   }
   Serial.println("Connected to WiFi");
 
+  // Obtener la dirección MAC después de la conexión a WiFi
+  macAddress = WiFi.macAddress();
+
   // Connect to MQTT broker
-  client.setServer(mqtt_server, mqtt_port);
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect("ESP32Client")) {
-      Serial.println("Connected to MQTT");
-    } else {
-      Serial.print("Failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-    }
+client.setServer(mqtt_server, mqtt_port);
+while (!client.connected()) {
+  Serial.println("Connecting to MQTT...");
+  // Intentar conectarse usando el usuario y la contraseña
+  if (client.connect("ESP32Client", mqtt_user, mqtt_password)) {
+    Serial.println("Connected to MQTT");
+    client.subscribe("checkpoint");
+    Serial.println("Subscribed to topic: checkpoint");
+  } else {
+    Serial.print("Failed with state ");
+    Serial.print(client.state());
+    delay(2000);
   }
+}
 
   // Initialize BLE
   BLEDevice::init("");
@@ -52,8 +60,6 @@ void loop() {
   // Scan for Bluetooth devices
   Serial.println("Scanning for BLE devices...");
   BLEScanResults *foundDevices = pBLEScan->start(scanTime, false);
-  
-
 
   // Prepare the message
   String message = "{\n  checkpointID: \"" + macAddress + "\",\n  animals: [\n";
@@ -61,7 +67,6 @@ void loop() {
   // Loop through found devices
   for (int i = 0; i < foundDevices->getCount(); i++) {
     BLEAdvertisedDevice device = foundDevices->getDevice(i);
-    String deviceName = device.getName().c_str();
     String deviceAddress = device.getAddress().toString().c_str();
     int rssi = device.getRSSI(); // Get the RSSI value
 
@@ -74,13 +79,14 @@ void loop() {
   }
 
   message += "\n  ]\n}";
+
   pBLEScan->clearResults(); // Clear scan results
 
   // Publish the list of devices to the MQTT topic
   if (client.connected()) {
-    client.publish("devices", devices.c_str());
+    client.publish("checkpoint", message.c_str()); // Publicar en el tema 'devices'
     Serial.println("Published to MQTT:");
-    Serial.println(devices);
+    Serial.println(message);
   } else {
     Serial.println("MQTT connection lost. Attempting to reconnect...");
     if (client.connect("ESP32Client")) {
