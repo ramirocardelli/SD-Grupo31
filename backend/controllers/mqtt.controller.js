57fdc04f6d3ab5a8fd7b80959e22df9392260b24
Bruno Trinitario <brunotrinitario@gmail.com>
2024-11-01 18:54:23 -0300

    [MQTT] guardar animales no registrados
    - cuando hace el filtrado para los animales que pertenecen a un checkpoint, analiza si no existe en nuestra BD y lo guarda en un vector

--- backend/controllers/mqtt.controller.js
+++ backend/controllers/mqtt.controller.js
@@ -1,12 +1,14 @@
 import mqtt from "mqtt";
 import { getAllCheckpoints } from "./checkpoints.controller.js";
-import { getAllAnimals, animalExists } from "./animals.controller.js";
+import { animalExists } from "./animals.controller.js";
+const avilableDevices = []
 const options = {
-  username: "xd",
-  password: "xd",
+  username: "admin",
+  password: "admin",
   clientID: "adminID",
 };
-const client = mqtt.connect("mqtt://192.168.0.247:1883", options);
+const mqttUrl = "mqtt://localhost:1883"
+const client = mqtt.connect(mqttUrl, options);
 const umbral = -50;
 //mapa = {checkpoint.id,[vector de animales]}
 const posiciones = new Map();
@@ -47,9 +49,7 @@ client.on("message", (topic, message) => {
     "[DEBUG]: " + JSON.stringify({ mensaje: messageString, topico: topic })
   );
   if (topic === "checkpoint") {
-    //console.log(message.toString());
-    actualizarPosicion(message.toString());
-    //console.log(posiciones);
+    actualizarPosicion(messageString);
     //client.end();
   }
 });
@@ -59,9 +59,17 @@ function actualizarPosicion(mensaje) {
   try {
     mensaje = JSON.parse(mensaje);
     const animalesRecibidos = mensaje?.animals;
-    const vec = animalesRecibidos.filter(
-      (animal) => animalExists(animal.id) && animal.rssi >= umbral
-    );
+    const vec = []
+    animalesRecibidos.forEach((animal) => {
+      if (animalExists(animal.id) && animal.rssi >= umbral) {
+        vec.push(animal);
+      } else {
+        if (!animalExists(animal.id) && !avilableDevices.includes(animal.id)){
+          // creen comveniente ponerle un timestamp?
+          avilableDevices.push(animal.id)
+        }
+      }
+    });
 
     if (mensaje?.checkpointID) {
       if (posiciones.has(mensaje.checkpointID)) {
@@ -71,6 +79,11 @@ function actualizarPosicion(mensaje) {
   } catch (e) {}
 }
 
+export function getAvilableAnimals(){
+  return avilableDevices
+}
+
 client.on("error", (err) => {
   //TODO que pasa si hay un error entre las raspberry y el broker
 });
+
