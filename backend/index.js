4f25f15ae8c76eb9e069956c7152ee5a156236f4
Bruno Trinitario <brunotrinitario@gmail.com>
2024-11-02 15:51:51 -0300

    Revert "[BUGFIX] para login y cors"
    This reverts commit 468f45d179bdb25d2ee3e267503491ac96ef55b4.

--- backend/index.js
+++ backend/index.js
@@ -32,12 +32,13 @@ connectToBroker();
 
 // usamos express y cors para poder comunicar back/front
 const app = express();
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 
+const setCorsHeaders = (res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+};
 
 // Raíz de la API
 const server = http.createServer((req, res) => {
@@ -124,11 +125,11 @@ function onLogin(req, res, body, pathArray) {
   const authHeader = req.headers['authorization'];
 
   if (!authHeader) {
-    return res.writeHead(401,'No se proporcionó encabezado de autorización');
+    return res.status(401).json({ message: 'No se proporcionó encabezado de autorización' });
   }
 
   if (pathArray.length > 1) {
-    return res.writeHead(401,'Path equivocado');
+    return res.status(401).json({ message: 'Path equivocado' });
   }
 
   const base64Credentials = authHeader.split(' ')[1];
