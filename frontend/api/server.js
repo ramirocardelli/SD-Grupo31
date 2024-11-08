68f362ec2585b6ea23a81db34bafd31c8787e8ab
Bruno Trinitario <brunotrinitario@gmail.com>
2024-11-08 19:17:30 -0300

    Borre archivos no utilizables

--- frontend/api/server.js
+++ frontend/api/server.js
@@ -1,10 +1,7 @@
 import express, { Router } from "express";
 import bodyParser from "body-parser";
 import "dotenv/config";
-import authRoutes from "./routes/AuthRoutes.js";
-import gamesRoutes from "./routes/GamesRoutes.js";
 import staticRoutes from "./routes/StaticRoutes.js";
-import userRoutes from "./routes/UserRoutes.js";
 import cors from "cors";
 
 const app = express();
@@ -15,9 +12,6 @@ app.post("/login", (req, res) => {
   console.log("Hola!");
 });
 app.use(bodyParser.json());
-app.use("/api", authRoutes);
-app.use("/api", gamesRoutes);
-app.use("/api", userRoutes);
 app.use(staticRoutes);
 
 app.listen(3001, () => {
--- frontend/frontend/static/js/helper/state/GamesStateHelper.js
+++
@@ -1,9 +0,0 @@
-export default class GamesStateHelper {
-    static getGames() {
-        return JSON.parse(sessionStorage.getItem('games'));
-    }
-
-    static setGames(games) {
-        sessionStorage.setItem('games', JSON.stringify(games));
-    }
-}
