08f7fc1ae87411e7f9dc42866023018128ada8a3
ramirocardelli <ramirocardelli89@gmail.com>
2024-11-09 20:18:12 -0300

    [Animals] Ahora se renderizan los módulos cuando ocurre algún evento

--- frontend/frontend/static/js/pages/AnimalPage.js
+++ frontend/frontend/static/js/pages/AnimalPage.js
@@ -4,15 +4,11 @@ import AuthStateHelper from "../helper/state/AuthStateHelper.js";
 export default class AnimalPage {
   constructor(selector) {
     this.container = document.getElementById(selector);
-    this.loadAnimal();
-  }
-
-  async loadAnimal() {
     this.render();
     this.addListener();
   }
 
-  async altaAnimal(event) {
+  altaAnimal = async (event) => {
     try {
       event.preventDefault();
 
@@ -38,7 +34,7 @@ export default class AnimalPage {
       if (response.ok) {
         //codigo de exito
         alert("Animal registrado exitosamente.");
-        event.target.reset()
+        event.target.reset();
       } else {
         alert("Error al registrar el animal: " + response.statusText);
       }
@@ -46,10 +42,10 @@ export default class AnimalPage {
       console.error("Error al dar de alta el animal:", error);
       alert("Ocurrió un error al registrar el animal.");
     }
-  }
+  };
 
   //SEND BAJA a la API (DELETE)
-  async bajaAnimal(animalId) {
+  bajaAnimal = async (animalId) => {
     try {
       const accessToken = AuthStateHelper.getAccessToken();
 
@@ -67,7 +63,7 @@ export default class AnimalPage {
       if (response.ok) {
         //codigo de exito
         alert("Animal eliminado exitosamente.");
-        this.listDevices();
+        this.renderPanelList();
       } else {
         alert("Error al eliminar el animal: " + response.statusText);
       }
@@ -75,10 +71,10 @@ export default class AnimalPage {
       console.error("Error al eliminar el animal:", error);
       alert("Ocurrió un error al eliminar el animal.");
     }
-  }
+  };
 
   //SEND MODIFY A LA API (PATCH)
-  async modifAnimal(event) {
+  modifAnimal = async (event) => {
     try {
       event.preventDefault();
 
@@ -104,46 +100,41 @@ export default class AnimalPage {
       if (response.ok) {
         //codigo de exito
         alert("Animal modificado exitosamente.");
-        this.listDevices();
+        this.renderPanelList();
       } else {
         alert("Error al modificar el animal: " + response.statusText);
       }
     } catch (error) {
-      console.log(error)
+      console.error("Error al modificar un animal:", error);
       alert("Ocurrió un error al modificar el animal.");
     }
-  }
+  };
 
   //TODO
-  async showAnimals() {
-    try{
+  showAnimals = async () => {
+    try {
       const map = L.map("map").setView(
         [-38.01200620443375, -57.581233775103186],
         13
       );
-  
+
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-  
+
       L.marker([-38.01200620443375, -57.581233775103186])
         .addTo(map)
         .bindPopup("A pretty CSS popup.<br> Easily customizable.")
         .openPopup();
-    }catch(e){}
-  }
+    } catch (e) {}
+  };
 
-  async listDevices() {
+  listDevices = async () => {
     const accessToken = AuthStateHelper.getAccessToken();
 
     const response = await AnimalAPIHelper.handleAnimal("get", "", accessToken);
-
     const listado = document.getElementById("listado");
-    listado.querySelectorAll("tr").forEach((tr) => {
-      tr.remove();
-    });
-
     const tr = document.createElement("tr");
     const tr_name = document.createElement("th");
     tr_name.textContent = "Nombre";
@@ -196,8 +187,7 @@ export default class AnimalPage {
       tr.appendChild(tr_modificar_rh);
       listado.appendChild(tr);
     });
-    showPanelAnimals("listar");
-  }
+  };
 
   //Manejo de las acciones
   handleSubmit = async (event) => {
@@ -224,80 +214,35 @@ export default class AnimalPage {
     }
   };
 
-  modificarAnimalId(id) {
+  modificarAnimalId = (id) => {
     const animalName = document.getElementById(`name-${id}`).textContent;
     const animalDescription = document.getElementById(
       `description-${id}`
     ).textContent;
-    showPanelAnimals("modificacion");
-    const inputId = document.getElementById("animalIdModif");
-    inputId.value = id;
-    const inputName = document.getElementById("animalNameModif");
-    inputName.value = animalName;
-    const inputDesc = document.getElementById("animalDescriptionModif");
-    inputDesc.value = animalDescription;
-  }
+    const animal = { id, animalName, animalDescription };
+    this.renderPanelModif(animal);
+  };
 
-  render() {
+  highlightButton = (buttonDataPanel) => {
+    const buttons = document.querySelectorAll(".sidebar-secundaria button");
+    buttons.forEach((button) => {
+      if (button.getAttribute("data-panel") === buttonDataPanel) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  };
+
+  render = () => {
     let homeHtml = `
-    
-               <div class="sidebar-secundaria">
-                    <button data-panel="alta" id="reg-animals-button" onclick="showPanelAnimals('alta')">Registrar animal</button>
-                    <button data-panel="mostrar" id="show-animals-button" onclick="showPanelAnimals('mostrar')">Mostrar animales</button>
-                    <button data-panel="listar" id="list-animals-button" >Listar animales</button>
-                </div>
-                <div class="page-container-animal">
-                </div>
-
-            <!-- alta -->
-            <div id="alta" class="alta-animal form-panel-animal" style="display: none;">
-                <h2>Registrar animal</h2>
-                <form class="login-form-animal">
-
-                    <!-- Listar animales disponibles -->
-                    <label for="list-mac-addr">Animales disponibles:</label>
-                    <select id="list-mac-addr" name="list-mac-addr" onchange="actualizarId()">
-                        <option selected="true" disabled="disabled">Collares disponibles</option>
-                    </select>
-
-                    <label for="identificador">Número de identificador de collar:</label>
-                    <input type="text" id="animalIdAlta" name="animalIdAlta" required>
-
-                    <label for="nombre">Nombre del animal:</label>
-                    <input type="text" id="animalNameAlta" name="animalNameAlta" required>
-
-                    <label for="nombre">Descripcion:</label>
-                    <input type="text" id="animalDescriptionAlta" name="animalDescriptionAlta" required>
-
-                    <div class="button-container-animal">
-                        <button type="submit" id="alta" class="alta-animal">Registrar</button>
-                    </div>
-
-                </form>
-            </div>
-
-            <!-- baja -->
-            <div id="baja" class="baja-animal form-panel-animal" style="display: none;">
-                <form class="login-form-animal">
-                    <h2>Eliminar animal</h2>
-
-                    <label for="baja-identificador">Número de identificador del collar:</label>
-                    <input type="text" id="animalIdBaja" name="animalIdBaja" required>
-
-                    <div class="button-container-animal">
-                        <button type="submit" id="baja" class="baja-animal">Eliminar</button>
-                    </div>
-
-                </form>
-            </div>
-
-            <!-- listar -->
-            <div id="listar" class="listar-animal form-panel-animal" style="display: none;">
-              <table id="listado" class="listado">
-              </table>
-            <form class="login-form-animal">  
-            </form>   
-            </div>
+              <div class="sidebar-secundaria">
+                <button data-panel="alta" id="reg-animals-button" onclick="">Registrar animal</button>
+                <button data-panel="mostrar" id="show-animals-button">Mostrar animales</button>
+                <button data-panel="listar" id="list-animals-button" >Listar animales</button>
+              </div>
+              <div id="page-container-animal">
+              </div>
 
             <!-- modificacion -->
             <div id="modificacion" class="form-panel-animal" style="display: none;">
@@ -318,27 +263,106 @@ export default class AnimalPage {
                     </div>
                 </form>
             </div>
-
-            <!-- Mostrar animales TODO MAPA -->
-            <div id="mostrar" class="form-panel-animal map-panel-animals" style="display: none;">
-                <h2>Mostrar animales en el Mapa</h2>
-                     <div id="map"></div>
-
-                    </div>
-                </div>
         `;
     this.container.innerHTML = homeHtml;
-  }
+  };
 
-  addListener() {
+  renderPanelList = () => {
+    this.highlightButton("listar");
+    const container = document.getElementById("page-container-animal");
+    container.innerHTML = `
+              <!-- listar -->
+              <div id="listar" class="listar-animal form-panel-animal">
+                <table id="listado" class="listado">
+                </table>
+              <form class="login-form-animal">  
+              </form>   
+              </div>
+                  `;
+    this.listDevices();
+  };
+
+  renderPanelMap = () => {
+    this.highlightButton("mostrar");
+    const container = document.getElementById("page-container-animal");
+    container.innerHTML = `
+            <!-- Mostrar animales -->
+            <div id="mostrar" class="form-panel-animal map-panel-animals">
+              <h2>Mostrar animales en el Mapa</h2>
+              <div id="map"></div>
+            </div>
+                  `;
+    this.showAnimals();
+  };
+
+  renderPanelAlta = () => {
+    this.highlightButton("alta");
+    const container = document.getElementById("page-container-animal");
+    container.innerHTML = `<div id="alta" class="alta-animal form-panel-animal">
+                  <h2>Registrar animal</h2>
+                  <form class="login-form-animal">
+  
+                      <!-- Listar animales disponibles -->
+                      <label for="list-mac-addr">Animales disponibles:</label>
+                      <select id="list-mac-addr" name="list-mac-addr" onchange="actualizarId()">
+                          <option selected="true" disabled="disabled">Collares disponibles</option>
+                      </select>
+  
+                      <label for="identificador">Número de identificador de collar:</label>
+                      <input type="text" id="animalIdAlta" name="animalIdAlta" required>
+  
+                      <label for="nombre">Nombre del animal:</label>
+                      <input type="text" id="animalNameAlta" name="animalNameAlta" required>
+  
+                      <label for="nombre">Descripcion:</label>
+                      <input type="text" id="animalDescriptionAlta" name="animalDescriptionAlta" required>
+  
+                      <div class="button-container-animal">
+                          <button type="submit" id="alta" class="alta-animal">Registrar</button>
+                      </div>
+                  </form>
+              </div>`;
+    this.listAvailableDevices();
+  };
+
+  renderPanelModif = (animal) => {
+    const container = document.getElementById("page-container-animal");
+    container.innerHTML = `
+            <!-- modificacion -->
+            <div id="modificacion" class="form-panel-animal">
+                <form class="login-form-animal">
+                    <h2>Modificar animal</h2>
+
+                    <label for="modificar-id">Id del animal:</label>
+                    <input type="text" id="animalIdModif" name="animalIdModif" value="${animal.id}" required>
+
+                    <label for="modificar-name">Nuevo nombre del animal:</label>
+                    <input type="text" id="animalNameModif" name="animalNameModif" value="${animal.animalName}" required>
+
+                    <label for="nuevo-nombre">Nueva descripcion:</label>
+                    <input type="text" id="animalDescriptionModif" name="animalDescriptionModif" value="${animal.animalDescription}" required>
+
+                    <div class="button-container-animal">
+                    <button type="submit" id="modif" class="modif-animal">Modificar</button>
+                    </div>
+                </form>
+            </div>`;
+  };
+
+  addListener = () => {
     this.container.addEventListener("submit", this.handleSubmit);
     document
       .getElementById("reg-animals-button")
-      .addEventListener("click", this.listAvailableDevices);
-    document.getElementById("list-animals-button").addEventListener("click", this.listDevices)
+      .addEventListener("click", this.renderPanelAlta);
 
-    document.getElementById("show-animals-button").addEventListener("click",this.showAnimals)
-  }
+    document
+      .getElementById("show-animals-button")
+      .addEventListener("click", this.renderPanelMap);
+
+    document
+      .getElementById("list-animals-button")
+      .addEventListener("click", this.renderPanelList);
+  };
 
   listAvailableDevices = async () => {
     const accessToken = AuthStateHelper.getAccessToken();
--- frontend/frontend/static/js/scripts/animals.js
+++ frontend/frontend/static/js/scripts/animals.js
@@ -1,32 +1,3 @@
-function showPanelAnimals(panelId) {
-  // Oculta todos los paneles
-  const panels = document.querySelectorAll(
-    ".form-panel-animal, .map-panel-animals"
-  ); //tmb ocultar mapa TODO
-  panels.forEach((panel) => {
-    panel.style.display = "none"; // Oculta el panel
-  });
-
-  // Muestra el panel seleccionado
-
-  const selectedPanel = document.getElementById(panelId);
-  if (selectedPanel) {
-    selectedPanel.style.display = "block";
-  }
-
-  // Cambia el estado de los botones
-  const buttons = document.querySelectorAll(
-    ".sidebar-secundaria-animal button"
-  );
-  buttons.forEach((button) => {
-    if (button.getAttribute("data-panel") === panelId) {
-      button.classList.add("active");
-    } else {
-      button.classList.remove("active");
-    }
-  });
-}
-
 function actualizarId() {
   var select = document.getElementById("list-mac-addr");
   var input = document.getElementById("animalIdAlta");
