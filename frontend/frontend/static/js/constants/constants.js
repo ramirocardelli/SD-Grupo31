8bde313bc684360b4af6dd53ad3070d27cc66a8c
Bruno Trinitario <brunotrinitario@gmail.com>
2024-11-08 23:17:28 -0300

    modularizacion
    no se enojen es para no olvidarme

--- frontend/frontend/static/js/constants/constants.js
+++ frontend/frontend/static/js/constants/constants.js
@@ -1,7 +1,7 @@
 export const CONSTANTS = {
   IP:"http://localhost:3000/API",
-  IP_ANIMALS:"http://localhost:3000/API/animals",
-  IP_LOGIN:"http://localhost:3000/API/login",
-  IP_REFRESH:"http://localhost:3000/API/refresh",
-  IP_CHECKPOINT:"http://localhost:3000/API/checkpoints",
+  IP_ANIMALS:IP+"/animals",
+  IP_LOGIN:IP+"/login",
+  IP_REFRESH:IP+"/refresh",
+  IP_CHECKPOINT:IP+"/checkpoints",
 };
